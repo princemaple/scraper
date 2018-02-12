48a8e4b4c076727c9486773f28b6606cf2bf5acf
Po Chen <chenpaul914@gmail.com>
2018-02-12 13:40:16 +1100

    refactor using with

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,12 +1,11 @@
 defmodule Scraper do
   def start_link(scraper_id, entries \\ [], opts \\ []) do
-    on_start =
-      [scraper_id: scraper_id]
-      |> Enum.into(opts)
-      |> Scraper.Supervisor.start_link()
-
-    Enum.each(entries, &Scraper.Scheduler.schedule(scraper_id, &1))
-
-    on_start
+    with {:ok, id} <-
+           [scraper_id: scraper_id]
+           |> Enum.into(opts)
+           |> Scraper.Supervisor.start_link() do
+      Enum.each(entries, &Scraper.Scheduler.schedule(scraper_id, &1))
+      {:ok, pid}
+    end
   end
 end
