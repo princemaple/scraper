fc0ff3f10eccd710cc8ce7e4b374a47055a8dfff
Po Chen <chenpaul914@gmail.com>
2018-02-12 13:44:19 +1100

    improve public api

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,10 +1,18 @@
 defmodule Scraper do
+  @spec start_link(
+    scraper_id :: atom,
+    entries :: String.t() | [String.t()],
+    opts :: Keyword.t()
+  ) :: GenServer.on_start()
   def start_link(scraper_id, entries \\ [], opts \\ []) do
     with {:ok, pid} <-
            [scraper_id: scraper_id]
            |> Enum.into(opts)
            |> Scraper.Supervisor.start_link() do
-      Enum.each(entries, &Scraper.Scheduler.schedule(scraper_id, &1))
+      entries
+      |> List.wrap()
+      |> Enum.each(&Scraper.Scheduler.schedule(scraper_id, &1))
+
       {:ok, pid}
     end
   end
