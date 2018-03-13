36b6507bc3fcae53d2a8bafd3d89b0891f651647
Po Chen <chenpaul914@gmail.com>
2018-03-13 23:09:33 +1100

    use link selectors to filter links

--- lib/scraper/worker.ex
+++ lib/scraper/worker.ex
@@ -23,15 +23,15 @@ defmodule Scraper.Worker do
 
         if state.link.depth < state.max_depth do
           links = Floki.attribute(html, "a", "href")
-          Enum.each(Enum.reject(links, &match?("#" <> _rest, &1)), fn link ->
-            Scraper.Scheduler.schedule(
-              state.scraper_id,
-              Scraper.Link.new(link, state.link)
-            )
-          end)
+
+          Enum.each(
+            select_links(links, link_selectors, state),
+            &Scraper.Scheduler.schedule(state.scraper_id, &1)
+          )
         end
 
         img_srcs = Floki.attribute(html, "img", "src")
+
         Enum.each(img_srcs, fn img_src ->
           state.data_store.put(
             {:via, Registry, {state.scraper_id, :data_store}},
@@ -45,4 +45,19 @@ defmodule Scraper.Worker do
 
     {:stop, :shutdown, state}
   end
+
+  defp select_links(links, link_selectors, state) do
+    links
+    |> Stream.reject(&match?("#" <> _rest, &1))
+    |> Stream.map(&Scraper.Link.new(&1, state.link))
+    |> Stream.filter(&apply_link_selectors(&1, link_selectors))
+  end
+
+  defp apply_link_selectors(link, link_selectors) do
+    Enum.all?(link_selectors, &apply_link_selector(link.url, &1))
+  end
+
+  defp apply_link_selector(url, {m, f, a}), do: apply(m, f, [url | a])
+  defp apply_link_selector(url, %Regex{} = regex), do: url =~ regex
+  defp apply_link_selector(url, f) when is_function(f), do: f.(url)
 end
