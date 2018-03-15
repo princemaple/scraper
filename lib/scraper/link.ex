d13dc5283d2ab5d103fdaf15e95055f671d96968
Po Chen <chenpaul914@gmail.com>
2018-03-15 22:40:44 +1100

    move link selection logic to Link module

--- lib/scraper/link.ex
+++ lib/scraper/link.ex
@@ -2,12 +2,12 @@ defmodule Scraper.Link do
   defstruct [:url, :depth, :referrer]
 
   @type t :: %__MODULE__{
-    url: String.t,
-    depth: non_neg_integer,
-    referrer: String.t
-  }
+          url: String.t(),
+          depth: non_neg_integer,
+          referrer: String.t()
+        }
 
-  @spec new(url :: String.t, referrer :: t) :: t
+  @spec new(url :: String.t(), referrer :: t) :: t
   def new(url, referrer) do
     %__MODULE__{
       url: fix(url, referrer),
@@ -16,7 +16,7 @@ defmodule Scraper.Link do
     }
   end
 
-  @spec fix(url :: String.t, referrer :: t) :: String.t
+  @spec fix(url :: String.t(), referrer :: t) :: String.t()
   def fix(url, referrer) do
     do_fix(url, URI.parse(referrer.url))
   end
@@ -26,4 +26,21 @@ defmodule Scraper.Link do
   defp do_fix("//" <> _rest = link, uri), do: uri.scheme <> ":" <> link
   defp do_fix("/" <> _rest = path, uri), do: URI.to_string(%{uri | path: path})
   defp do_fix(link, uri), do: URI.to_string(%{uri | path: Path.join(uri.path, link)})
+
+  @spec select(links :: [String.t()], selectors :: [{module, atom, [any]} | Regex.t() | function]) ::
+          [String.t()]
+  def select(links, selectors, referrer_link) do
+    links
+    |> Stream.reject(&match?("#" <> _rest, &1))
+    |> Stream.map(&Scraper.Link.new(&1, referrer_link))
+    |> Stream.filter(&apply_link_selectors(&1, selectors))
+  end
+
+  defp apply_link_selectors(link, selectors) do
+    Enum.all?(selectors, &apply_link_selector(link.url, &1))
+  end
+
+  defp apply_link_selector(url, {m, f, a}), do: apply(m, f, [url | a])
+  defp apply_link_selector(url, %Regex{} = regex), do: url =~ regex
+  defp apply_link_selector(url, f) when is_function(f), do: f.(url)
 end
--- lib/scraper/worker.ex
+++ lib/scraper/worker.ex
@@ -25,7 +25,7 @@ defmodule Scraper.Worker do
           links = Floki.attribute(html, "a", "href")
 
           Enum.each(
-            select_links(links, link_selectors, state),
+            Scraper.Link.select(links, link_selectors, state.link),
             &Scraper.Scheduler.schedule(state.scraper_id, &1)
           )
         end
@@ -45,19 +45,4 @@ defmodule Scraper.Worker do
 
     {:stop, :shutdown, state}
   end
-
-  defp select_links(links, link_selectors, state) do
-    links
-    |> Stream.reject(&match?("#" <> _rest, &1))
-    |> Stream.map(&Scraper.Link.new(&1, state.link))
-    |> Stream.filter(&apply_link_selectors(&1, link_selectors))
-  end
-
-  defp apply_link_selectors(link, link_selectors) do
-    Enum.all?(link_selectors, &apply_link_selector(link.url, &1))
-  end
-
-  defp apply_link_selector(url, {m, f, a}), do: apply(m, f, [url | a])
-  defp apply_link_selector(url, %Regex{} = regex), do: url =~ regex
-  defp apply_link_selector(url, f) when is_function(f), do: f.(url)
 end
